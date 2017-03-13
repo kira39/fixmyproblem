24288ed5c292f8b106092922a31e319bdeed5e40
Emily Chen <Emily@Emilys-MacBook-Pro-5.local>
2017-03-12 21:29:39 -0500

    Creating login and logout functionality

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', '127.0.0.1');
+   define('DB_USERNAME', 'jms395');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'fixmyproblem');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
